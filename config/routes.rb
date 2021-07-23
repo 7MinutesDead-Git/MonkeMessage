ea1c0030f0d36a59cce454e5da7b76cb2028ea05
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-07-23 12:50:13 -0500

    Add REST/CRUD notes to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,21 @@
 # - If done this way, under "views", rails will expect:
 #   - A "pages" folder (named for pages_controller) and
 #   - A "home.html.erb" template (named for home action).
+#
+# REST means Representational State Transfer -
+#   Mapping HTTP verbs:
+#     - Frontend requests (get, post, delete, etc)
+#   to CRUD actions:
+#     - Backend methods (show, index, destroy, etc).
 
 Rails.application.routes.draw do
   # The default page to load.
   root('pages#home')
   # Load about page when requesting /about.
   get('about', to:'pages#about')
+
   # Article resources.
+  # Resources provide RESTful routes to Rails resources.
   # "only:" means only generate routes for the given actions.
   # https://guides.rubyonrails.org/routing.html#crud-verbs-and-actions
   resources(:articles, only: [:show, :index, :new, :create, :edit, :update, :destroy])
