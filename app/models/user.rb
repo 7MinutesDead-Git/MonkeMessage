075605a1e5faf3ad5c5b18ffc3d5b7a76d6ff6dc
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-11-04 08:00:37 -0500

    Add User validations for username and email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
+  # Verify entry with .valid?.
+  # After .valid? check (or .save attempt), if false, then .errors.full_messages can be checked.
+
+  # Validation: Present, unique, valid length.
+  # Uniqueness defaults to true, but we can specify false for case sensitivity specifically.
+  validates(:username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 })
+
+  validates(:email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            # Verify email with ruby's standard library regex.
+            format: { with: URI::MailTo::EMAIL_REGEXP })
 
 end
+
