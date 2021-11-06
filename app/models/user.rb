e44f9a70db93f55e093e84bc991b9799f977f558
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-11-06 13:52:34 -0500

    Ensure emails are lowercase before saved into database.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
+# Users who can login, send and receive messages, etc.
 class User < ApplicationRecord
+  # Each user can create many articles/messages.
   has_many(:articles)
 
-  # Verify entry with .valid?.
-  # After .valid? check (or .save attempt), if false, then .errors.full_messages can be checked.
-
   # Validation: Present, unique, valid length.
-  # Uniqueness defaults to true, but we can specify false for case sensitivity specifically.
   validates(:username,
             presence: true,
+            # Uniqueness defaults to true, but we can also specify false for case-sensitivity only.
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 })
 
@@ -17,6 +16,10 @@ class User < ApplicationRecord
             length: { maximum: 105 },
             # Verify email with ruby's standard library regex.
             format: { with: URI::MailTo::EMAIL_REGEXP })
+
+  # Ensure all emails are saved as lowercase for simplicity and consistency.
+  # Note that before_save is only called after validation has passed.
+  before_save { self.email = email.downcase }
 
 end
 
