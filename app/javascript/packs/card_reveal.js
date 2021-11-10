b0ba59d614ee227a234d08484d7880883b3fa83b
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-11-10 15:28:25 -0600

    Fixed looping bug breaking anims. Removed else statement, as we really don't need to re-hide the messages.

--- app/javascript/packs/card_reveal.js
+++ app/javascript/packs/card_reveal.js
@@ -1,14 +1,11 @@
+
 function fadeCardsIn() {
     const window_top = $(window).scrollTop()
     const window_bottom = $(window).scrollTop() + $(window).innerHeight()
+    const cards = $(".card")
 
-    console.log(`Viewport: ${window_top} to ${window_bottom}.`)
-
-    var cards = $(".card")
-    var count = 0
-
+    // Add event listener to each card
     cards.each(function() {
-        count += 1
         const card = $(this)
 
         const card_top = card.offset().top;
@@ -16,20 +13,11 @@ function fadeCardsIn() {
 
         // If our card is in the range of the window viewport.
         if ((window_bottom > card_top) && (window_top < card_bottom)) {
-            card.fadeTo(1000, 1, "swing")
-            console.log(`Showing card ${count}.`)
-        } else {
-            card.fadeTo(1000, 0, "swing")
+            card.fadeTo(500, 1, "swing")
         }
-
-        // if (bottomOfCurrentView > cardDistanceToTop) {
-        //     card.fadeTo('slow', 1, "swing");
-        //     console.log(`Fading in card ${count}.`)
-        // } else {
-        //     card.fadeTo('slow', 0, "swing");
-        // }
     })
 }
 
+// Add event listener to each card, that fires when element is in view.
 $(document).ready(fadeCardsIn)
 $(window).scroll(fadeCardsIn)
