a59e62d5e9498388187525595c9d1e05b447b201
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-12-14 15:48:27 -0600

    Add admin functionality.

---
+++ db/migrate/20211214204251_add_admin_to_users.rb
@@ -0,0 +1,6 @@
+# DB Migration: Adding new admin boolean to users table.
+class AddAdminToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column(:users, :admin, :boolean, default: false)
+  end
+end
