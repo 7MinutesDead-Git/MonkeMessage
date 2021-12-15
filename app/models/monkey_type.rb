f454aa206d622d6a22c2dbf6787c758c3a13f8cc
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-12-14 19:27:01 -0600

    Remove old code comment.

--- app/models/monkey_type.rb
+++ app/models/monkey_type.rb
@@ -7,8 +7,6 @@ class MonkeyType < ApplicationRecord
   FRIENDLY_MIN = 0
   FRIENDLY_MAX = 10
 
-  # attr_reader :age_min, :age_max, :name_min_length, :name_max_length, :friendly_min, :friendly_max
-
   validates :name, presence: true, length: { minimum: NAME_MIN_LENGTH, maximum: NAME_MAX_LENGTH }
   validates :age, numericality: true, inclusion: { in: AGE_MIN..AGE_MAX }
   validates :friendliness, inclusion: { in: FRIENDLY_MIN..FRIENDLY_MAX }
