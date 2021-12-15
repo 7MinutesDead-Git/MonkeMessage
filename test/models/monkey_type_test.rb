e78a3e0a903e6bfe3810d4b5944925413985fd9f
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-12-14 19:19:10 -0600

    Create MonkeyType tests.

--- test/models/monkey_type_test.rb
+++ test/models/monkey_type_test.rb
@@ -3,8 +3,50 @@
 require 'test_helper'
 
 class MonkeyTypeTest < ActiveSupport::TestCase
+  # ----------------------------------------
+  # Setup is run before each test.
+  def setup
+    @monkey_type = MonkeyType.new(name: 'emperor tamarin', age: 7, friendliness: 9)
+  end
+  # ----------------------------------------
   test 'monkey type should be valid' do
-    @monkey_type = MonkeyType.new(name: 'emperor tamarin')
     assert(@monkey_type.valid?)
+  end
+
+  # ----------------------------------------
+  test 'monkey type name should be present' do
+    @monkey_type.name = '    '
+    assert_not @monkey_type.valid?
+  end
+
+  # ----------------------------------------
+  test 'monkey type friendliness should not exceed maximum' do
+    @monkey_type.friendliness = MonkeyType::AGE_MAX + 1
+    assert_not @monkey_type.valid?
+  end
+
+  # ----------------------------------------
+  test 'monkey type friendliness should not be below zero' do
+    @monkey_type.friendliness = MonkeyType::FRIENDLY_MIN - 1
+    assert_not @monkey_type.valid?
+  end
+
+  # ----------------------------------------
+  test 'monkey type name should be unique' do
+    @monkey_type.save
+    @monkey_type_two = MonkeyType.new(name: 'emperor tamarin')
+    assert_not @monkey_type_two.valid?
+  end
+
+  # ----------------------------------------
+  test 'monkey type name should be not be excessively long' do
+    @monkey_type.name = 'm' * (MonkeyType::NAME_MAX_LENGTH + 1)
+    assert_not @monkey_type.valid?
+  end
+
+  # ----------------------------------------
+  test 'monkey type name should be not be excessively short' do
+    @monkey_type.name = MonkeyType::NAME_MIN_LENGTH - 1
+    assert_not @monkey_type.valid?
   end
 end
