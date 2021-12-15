2ac36b4e8d8835e39254960f712dc68b1779a5be
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-12-14 19:18:53 -0600

    Create MonkeyType validations and constants.

--- app/models/monkey_type.rb
+++ app/models/monkey_type.rb
@@ -1,4 +1,16 @@
-
+# The type of monkey a Monke can be. Includes age and friendliness.
 class MonkeyType < ApplicationRecord
+  AGE_MIN = 0
+  AGE_MAX = 300
+  NAME_MIN_LENGTH = 3
+  NAME_MAX_LENGTH = 50
+  FRIENDLY_MIN = 0
+  FRIENDLY_MAX = 10
 
+  # attr_reader :age_min, :age_max, :name_min_length, :name_max_length, :friendly_min, :friendly_max
+
+  validates :name, presence: true, length: { minimum: NAME_MIN_LENGTH, maximum: NAME_MAX_LENGTH }
+  validates :age, numericality: true, inclusion: { in: AGE_MIN..AGE_MAX }
+  validates :friendliness, inclusion: { in: FRIENDLY_MIN..FRIENDLY_MAX }
+  validates_uniqueness_of :name
 end
