7041a7ea9750b5daffc291d094cdbb9bde36ef20
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-12-19 19:35:24 -0600

    MonkeyTypes: Create "create" action and params permissions.

--- app/controllers/monkey_types_controller.rb
+++ app/controllers/monkey_types_controller.rb
@@ -1,14 +1,38 @@
 class MonkeyTypesController < ApplicationController
+  # ----------------------
   def new
     @monkey_type = MonkeyType.new
   end
 
+  # ----------------------
   def show
   end
 
+  # ----------------------
   def index
   end
 
+  # ----------------------
   def create
+    @monkey_type = MonkeyType.new(monkey_type_params)
+
+    if @monkey_type.save
+      flash[:notice] = "Added new Monke! [ #{@monkey_type.name} ]"
+      redirect_to @monkey_type
+    else
+      render 'new'
+    end
+  end
+
+  # ----------------------
+  private
+
+  def monkey_type_params
+    params.require(:monkey_type).permit(
+      :name,
+      :scientific_name,
+      :colloquial_name,
+      :age,
+      :friendliness)
   end
 end
