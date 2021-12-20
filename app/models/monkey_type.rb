231bbfde730f0eade892c7776d8d55a78fc325a5
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-12-19 19:35:39 -0600

    MonkeyTypes: Validate length of scientific and colloquial names.

--- app/models/monkey_type.rb
+++ app/models/monkey_type.rb
@@ -1,13 +1,15 @@
 # The type of monkey a Monke can be. Includes age and friendliness.
 class MonkeyType < ApplicationRecord
   AGE_MIN = 0
-  AGE_MAX = 300
+  AGE_MAX = 10000
   NAME_MIN_LENGTH = 3
   NAME_MAX_LENGTH = 50
   FRIENDLY_MIN = 0
   FRIENDLY_MAX = 10
 
   validates :name, presence: true, length: { minimum: NAME_MIN_LENGTH, maximum: NAME_MAX_LENGTH }
+  validates :scientific_name, length: { minimum: NAME_MIN_LENGTH, maximum: NAME_MAX_LENGTH }
+  validates :colloquial_name, length: { minimum: NAME_MIN_LENGTH, maximum: NAME_MAX_LENGTH }
   validates :age, numericality: true, inclusion: { in: AGE_MIN..AGE_MAX }
   validates :friendliness, inclusion: { in: FRIENDLY_MIN..FRIENDLY_MAX }
   validates_uniqueness_of :name
