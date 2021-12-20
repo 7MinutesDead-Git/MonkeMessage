d6038d379a34ea585b34f425b4072aaf866e45b5
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-12-19 21:07:36 -0600

    Create integration test for full monkey_type creation process.

--- test/integration/create_monkey_type_test.rb
+++ test/integration/create_monkey_type_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class CreateMonkeyTypeTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  # ------------------------------------------------------------
+  test "get new monkey_type form and create monkey_type" do
+    # Test for getting the form.
+    get "/monkey_types/new"
+    assert_response :success
+
+    # Test for successfully creating a new monkey type.
+    assert_difference 'MonkeyType.count', 1 do
+      post(monkey_types_path, params: MonkeyTypeTest::SQUIRREL_TYPE_PARAMS )
+      assert_response :redirect
+    end
+
+    # Test for redirection to new monkey page after.
+    follow_redirect!
+    assert_response :success
+    # Monkey name should show up somewhere in the html body.
+    assert_match "central american squirrel", response.body
+  end
 end
